28bed747c817c453d04233a4cb5d93f4cf8c706b
luchi <39061286+luyangchi@users.noreply.github.com>
2026-04-17 23:14:37 -0400

    Refactor database connection to use environment variables

--- db.php
+++ db.php
@@ -1,10 +1,11 @@
 <?php
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "engine_sim";
+$host = getenv("MYSQLHOST");
+$user = getenv("MYSQLUSER");
+$password = getenv("MYSQLPASSWORD");
+$database = getenv("MYSQLDATABASE");
+$port = getenv("MYSQLPORT");
 
-$conn = new mysqli($host, $user, $password, $database);
+$conn = new mysqli($host, $user, $password, $database, $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
