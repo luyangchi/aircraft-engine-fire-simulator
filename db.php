7d775fbe51c4185ed92f48051dd6b0bd2c0a0469
luchi <39061286+luyangchi@users.noreply.github.com>
2026-04-17 23:38:17 -0400

    Cast port to integer for database connection

--- db.php
+++ db.php
@@ -5,7 +5,7 @@ $password = getenv("MYSQLPASSWORD");
 $database = getenv("MYSQLDATABASE");
 $port = getenv("MYSQLPORT");
 
-$conn = new mysqli($host, $user, $password, $database, $port);
+$conn = new mysqli($host, $user, $password, $database, (int)$port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
